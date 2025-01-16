4bea8a4d9fc06a43217e594f4c51bba2134c0ac9
Kirill <kir.karasyov2017@yandex.ru>
2025-01-16 20:55:32 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/syferever/TwoSide_Litho_gui
+
+go 1.23.4
---
+++ twoside_litho.go
@@ -0,0 +1,16 @@
+package github.com/syferever/TwoSide_Litho_gui
+
+import (
+	"log"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
+	"periph.io/x/host/v3"
+	rg "github.com/gen2brain/raylib-go/raygui"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+func main() {
+
+
+	rl.WindowInit
+}
